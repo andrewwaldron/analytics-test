0746dd16cf8ed0d40f2efff35b79b306f8af49ad
cah-andrewwaldron <andrew.waldron@cardinalhealth.com>
2014-10-08 16:53:48 -0400

    Added a basline version of the page to test with to see what the user clicks on and start testing the analytics frameworks

---
+++ angular/assets/js/app.js
@@ -0,0 +1,30 @@
+var app = angular.module("App", ["ngRoute"]);
+
+app.config(['$routeProvider', function($routeProvider) {
+	$routeProvider
+		.when('/home', {
+			templateUrl: 'partials/index.htm',
+			controller: "HomeController"
+		})
+	    .when('/team/:teamId?', {
+			templateUrl: 'partials/team.htm',
+			controller: "TeamsController"
+	    })
+	    .otherwise({ redirectTo: "/home" });
+}]);
+
+
+app.controller('HomeController', ['$scope', function ($scope) {
+	$scope.topScores = topTeams;
+}]);
+
+app.controller('TeamsController', ['$scope', '$routeParams', function ($scope, $routeParams) {
+	if ($routeParams.teamId) {
+		// list single team
+		$scope.teamDetails = topTeams[$routeParams.teamId - 1];
+		$scope.teamId = $routeParams.teamId;
+	} else {
+		// list all teams
+		$scope.teams = topTeams;
+	}
+}]);
---
+++ angular/assets/js/sampledata.js
@@ -0,0 +1,8 @@
+var topTeams = [
+		{teamId: 1, teamName: "DCP", score: 1293},
+		{teamId: 2, teamName: "DCP2", score: 48},
+		{teamId: 3, teamName: "DCP3", score: 93},
+		{teamId: 4, teamName: "DCP4", score: 245},
+		{teamId: 5, teamName: "DCP5", score: 3},
+		{teamId: 6, teamName: "DCP6", score: 234}
+	];
